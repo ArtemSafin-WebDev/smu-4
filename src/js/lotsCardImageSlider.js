f6665262886c0547aa6e01e277a573598b5a6e65
Артем Сафин <artemsafin.webdev@gmail.com>
2020-11-14 09:42:08 +0300

    Смена слайдов при ведении мышкой

--- src/js/lotsCardImageSlider.js
+++ src/js/lotsCardImageSlider.js
@@ -7,8 +7,9 @@ export default function LotsCardImageSlider() {
 
     elements.forEach(element => {
         const container = element.querySelector('.swiper-container');
-        
-        new Swiper(container, {
+        let trackingCursor = false;
+        let currentSlideIndex;
+        const instance = new Swiper(container, {
             effect: 'fade',
             watchOverflow: true,
             fadeEffect: {
@@ -18,6 +19,37 @@ export default function LotsCardImageSlider() {
                 el: element.querySelector('.lots__card-image-slider-pagination'),
                 type: 'bullets',
                 clickable: true
+            },
+            on: {
+                slideChange: function() {
+                    currentSlideIndex = this.realIndex;
+                    console.log('Current slide', currentSlideIndex)
+                }
+            }
+        });
+        const slidesCount = instance.slides.length;
+
+
+        container.addEventListener('mouseenter', ()=> {
+            trackingCursor = true;
+        })
+        container.addEventListener('mouseleave', () => {
+            trackingCursor = false;
+        })
+
+        container.addEventListener('mousemove', event => {
+            if (!trackingCursor) return;
+            const { offsetX, target } = event;
+            const width = target.offsetWidth;
+            const progress = Math.ceil(offsetX / width * slidesCount);
+            let activeSlideIndex = progress - 1;
+            
+            console.log({
+                progress,
+            })
+           
+            if (activeSlideIndex !== currentSlideIndex) {
+                instance.slideTo(activeSlideIndex);
             }
         });
     });
